4245e92e03e5ba43bff9a64f6c859f0f0c95cd25
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-23 02:03:52 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// Masala: oy raqami berilgan kiritilga oy qaysi faslga tegishli ekanini aniqlang
+
+let n=10;
+
+switch(n){
+    case (n=1):
+        console.log("qish");
+        break;
+
+    case (n=2):
+        console.log("qish");
+        break;
+
+    case (n=3):
+        console.log("Bahor");
+        break;
+
+    case (n=4):
+        console.log("Bahor");
+        break;
+
+    case (n=5):
+        console.log("Bahor");
+        break;
+
+    case (n=6):
+        console.log("Yoz");
+        break;
+
+    case (n=7):
+        console.log("Yoz");
+        break;
+
+    case (n=8):
+        console.log("Yoz");
+        break;
+
+    case (n=9):
+        console.log("Kuz");
+        break;
+
+    case (n=10):
+        console.log("Kuz");
+        break;
+
+    case (n=11):
+        console.log("Kuz");
+        break;
+
+    case (n=12):
+        console.log("Qish");
+        break;
+    
+}
